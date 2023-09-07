9e625f772322dc3abad02c706378d271d67e704d
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-07 17:34:36 +0100

    prmise

--- src/Practice.js
+++ src/Practice.js
@@ -1,7 +1,21 @@
- import{useState} from "react";
- export default function Practice(){
-    const MyData="lknoiio"
-    return(
-        <div>Promise lkdfhslhf</div>
+import { useEffect, useState } from "react";
+export default function Practice() {
+    const [value, setValue] = useState()
+    const MyData = (a, b) => {
+        return new Promise((resolve, rejct) => {
+            const sub = a - b;
+            resolve(sub);
+        })
+    }
+    useEffect(() => {
+        async function Main() {
+            await MyData(10, 2).then((result) => {
+                setValue(result);
+            })
+        }
+        Main();
+    })
+    return (
+        <div>Promise lkdfhslhf {value}</div>
     )
 }
