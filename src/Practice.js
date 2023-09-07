d58a04cbe91b2594d346e8822a1e11707470e659
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-07 17:13:53 +0100

    promise

--- src/Practice.js
+++ src/Practice.js
@@ -1,7 +1,26 @@
- import{useState} from "react";
+ import{useState, useEffect} from "react";
+
  export default function Practice(){
-    const MyData="lknoiio"
+    const[value,setValue]= useState();
+    const MyData=(a,b)=>{
+        return new Promise ((resolve,reject)=>{
+            const sub = a-b;
+            resolve(sub);
+        })
+    }
+    useEffect(()=>{
+       async  function Main(){
+       await MyData(10,2).then((result)=>{
+        setValue(result)
+       })
+       }
+       Main();
+    },[])
+
     return(
-        <div>Promise</div>
+        <div> the Promise of subtraction is:
+            hello world
+           {value}
+        </div>
     )
 }
