8d451325c61430fb6e75ecfdc8cceabfc8e99ca7
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-11 17:12:14 +0100

    complition of form

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { FcCalendar } from "react-icons/fc";
 import 'react-calendar/dist/Calendar.css';
 export default function Form() {
-    const[sucessMsg,setSucessMsg]=useState("")
+    const [sucessMsg, setSucessMsg] = useState("")
     const [formData, setFormData] = useState({
         fname: '',
         lname: '',
@@ -12,99 +12,104 @@ export default function Form() {
         calendar: '',
         Date: ''
     });
-    const[error,setError]=useState({
+    const [error, setError] = useState({
         fname: '',
         lname: '',
         cars: '',
         email: '',
         calendar: '',
-        Date: ''  
+        Date: ''
     })
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-        if(formValidation()){
+        if (formValidation()) {
             setSucessMsg("Thank you for sharing details!!, we will get back to you soon.");
-        setFormData({
-         fname:'',
-         laname:'',
-         cars:'',
-         email:'',
-         calendar:'',
-         Date:'',
+            setFormData({
+                fname: '',
+                lname: '',
+                cars: '',
+                email: '',
+                calendar: '',
+                Date: '',
 
-        })
-        setTimeout(() => {
-            setSucessMsg('');
-        }, 3000);}
+            })
+            setTimeout(() => {
+                setSucessMsg('');
+            }, 3000);
+        }
         else {
             setSucessMsg("form is not valid")
         }
     }
-    const handleChange=(e)=>{
-const name= e.target.name;
-const value= e.target.value;
-setFormData((prevData)=>({
-    ...prevData,
-    [name]:value
-}))
-formValidation();
-} 
-   const formValidation=()=>{
-    let isValid = true;
-    const newErrors ={...error}
-    if(newErrors.fname.trim()===''){
-        newErrors.fname='fname is required';
-        isValid = false;
+    const handleChange = (e) => {
+        debugger;
+        const name = e.target.name;
+        const value = e.target.value;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }))
+        formValidation();
+        // console.log(formData)
     }
-    else{
-        newErrors.faname='';
-    }
-    if(newErrors.lname.trim()===''){
-        newErrors.lname='lname is required';
-        isValid =false;
-    }
-    else{
-        newErrors.lname=''
-    }
-    if(newErrors.email.trim()===''){
-        newErrors.email='email is required';
-        isValid=false;
-    }
-    else{
-        newErrors.email='';
-    }
-    if(newErrors.calendar.trim()===''){
-        newErrors.calendar='select the required date';
-        isValid =false;
-    }
-    else{
-        newErrors.calendar=''
-    }
-    setError(newErrors);
-return isValid;
+    const formValidation = () => {
+        debugger;
+        let isValid = true;
+        const newErrors = { ...error }
+        if (formData.fname.trim() === '') {
+            newErrors.fname = 'fname is required';
+            isValid = false;
+        }
+        else {
+            newErrors.fname = '';
+        }
+        if (formData.lname.trim() === '') {
+            newErrors.lname = 'lname is required';
+            isValid = false;
+        }
+        else {
+            newErrors.lname = ''
+        }
+        if (formData.email.trim() === '') {
+            newErrors.email = 'email is required';
+            isValid = false;
+        }
+        else {
+            newErrors.email = '';
+        }
+        if (formData.calendar.trim() === '') {
+            newErrors.calendar = 'select the required date';
+            isValid = false;
+        }
+        else {
+            newErrors.calendar = ''
+        }
+        setError(newErrors);
+        return isValid;
 
-   }
+    }
     const [date, setDate] = useState(new Date());
     const [show, setShow] = useState(false);
-    useEffect(()=>{
-        formData.date= formatDate(date)
+    useEffect(() => {
+        formData.Date = formatDate(date)
+        setShow(false);
 
-    },[date,formData]);
-    
-  
-    const formatDate =(d)=>{
-        if(!d) return '';
-        var month = ''+(d.getMonth()+1);
-        var day = ''+ d.getDay();
-        var year =''+d.getFullYear();
-        if(month.length<2){
-            month = '0'+month;
+    }, [date, formData]);
+
+
+    const formatDate = (d) => {
+        if (!d) return '';
+        var month = '' + (d.getMonth() + 1);
+        var day = '' + d.getDay();
+        var year = d.getFullYear();
+        if (month.length < 2) {
+            month = '0' + month;
         }
-        if(day.length<2){
-            day = '0'+day;
+        if (day.length < 2) {
+            day = '0' + day;
         }
-       
+        return [month, day, year].join('/');
     }
 
 
@@ -114,16 +119,33 @@ return isValid;
             <div className="row px-5 py-5">
                 Form:
                 <form>
-                    <div><label for="fname"  className="form-label"><h5>FNAME:</h5></label>
-                        <input type="fname" onChange={handleChange} className={`form-control ${error.fname.length?'error-field':''} ${formData.fname?'sucess':''}`} id="exampleFormCintrolInput1" placeholder="fname" />
-                       <p className="popup">{error.fname.length>0?error.fname:null}</p>
-                       <label for="lname" className="form-label"><h5>LNAME:</h5></label>
-                        <input type="lname"  className="form-control" id="exampleFormCintrolInput1" placeholder="lname" /></div>
-
+                    <div><label for="fname" className="form-label"><h5>FNAME:</h5></label>
+                        <input
+                            type="text"
+                            onChange={handleChange}
+                            className={`form-control ${error.fname.length ? 'error-field' : ''} ${formData.fname ? 'sucess' : ''}`} placeholder="fname"
+                            area-label="first name"
+                            name='fname'
+                            value={formData.fname}
+                        />
+                    </div>
+                    <p className="popup">{error.fname.length > 0 ? error.fname : null}</p>
+                    <label for="exampleFormCintrolInput1" className="form-label"><h5>LNAME:</h5></label>
+                    <div>
+                        <input
+                            type="text"
+                            onChange={handleChange}
+                            className={`form-control ${error.lname.length ? 'error-field' : ''} ${formData.lname ? 'sucess' : ''}`}
+                            id="exampleFormCintrolInput1"
+                            placeholder="lname" name='lname'
+                            value={formData.lname}
+                        />
+                    </div>
+                    <p className="popup">{error.lname.length > 0 ? error.lname : null}</p>
                     <label for="exampleFormControlSelect1">Example Multiple Select Cars:</label>
-                    <div className="form-group ">
+                    <div className="form-group " onChange={handleChange}>
 
-                        <select className="from-control w-100" id="exampleFormControlSelect1">
+                        <select className={`from-control w-100 `} id="exampleFormControlSelect1">
                             <option>audi</option>
                             <option>Maruti</option>
                             <option>ballino</option>
@@ -134,12 +156,15 @@ return isValid;
                         </select>
                     </div>
                     <div><label for="email" className="form-label"><h5>email:</h5></label>
-                        <input type="email" className="form-control" id="exampleFormCintrolInput1" placeholder="email" /></div>
-                    <div><label for="calendar" className="form-label"><h5>Choose Date:</h5></label>
-                        <input type="text" className="form-control" id="datepicker" placeholder="Avilability" onClick={()=>{setShow(!show)}} />
-                        <span className="cal-icon w-50" onClick={()=>{setShow(!show)}}>{show&&<Calendar onChang={setDate} value={date} />} <FcCalendar /></span></div>
+                        <input type="email" onChange={handleChange} className={`form-control ${error.email.length ? 'error-field' : ''} ${formData.email ? 'sucess' : ''}`} id="exampleFormCintrolInput1" placeholder="email" name='email' value={formData.email} /></div>
 
-                    <button type="submit mt-2" className="custom-button">Check Avilability</button>
+                    <div><label for="calendar" onChange={handleChange} className="form-label"><h5>Choose Date:</h5></label>
+                        <input type="text" className="form-control" id="datepicker" placeholder="Avilability" name='Avilability' value={formData.Date} onClick={() => { setShow(!show) }} />
+                        <span className="cal-icon w-60" > <FcCalendar /></span>
+                        {show && <Calendar onChange={setDate} value={date} name="Avilability" />}</div>
+
+                    <button type="submit mt-2" className="custom-button" onClick={handleSubmit}>Check Avilability</button>
+                    <p className="message">{sucessMsg.length ? 'message' : null}</p>
                 </form>
             </div>
         </div>
