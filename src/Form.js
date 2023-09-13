c5868ed8ad9f18cb6d09ee1051164d83b689a730
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-13 11:02:03 +0100

    form changes

--- src/Form.js
+++ src/Form.js
@@ -102,13 +102,13 @@ export default function Form() {
     const formatDate = (d) => {
         if (!d) return '';
         var month = '' + (d.getMonth() + 1);
-        var day =  '' + d.getDate();
+        var day = '' + d.getDate();
         var year = d.getFullYear();
         if (month.length < 2) {
             month = '0' + month;
         }
         if (day.length < 2) {
-            day =  '0'+ day;
+            day = '0' + day;
         }
         return [day, month, year].join('/');
     }
