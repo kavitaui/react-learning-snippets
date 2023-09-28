a41b3fc63747705ca93ecebf955423654a45f6b8
kavitaui <122557323+kavitaui@users.noreply.github.com>
2023-09-28 18:55:31 +0100

    animation learn to make crousel

--- src/AnimationCrousel.js
+++ src/AnimationCrousel.js
@@ -1,6 +1,39 @@
+import Mycard from "./Mycard"
 export default function Crousel(){
+    
+        let box = document.querySelector('.product-container');
+        const btnpressprev =()=>{
+           let width = box.clientWidth;
+           box.scrollLeft =box.scrollLeft -width;
+           console.log(width)
+        }
+        const btnpressnxt =()=>{
+            let width = box.clientWidth;
+            box.scrollLeft = box.scrollLeft+width;
+            console.log(width);
+        }
+    
     return(
-    <div>
-       My Crousel </div>
+    <div className="product-crousel">
+        <button className="prev-btn" onClick={btnpressprev}>&lt;<p></p></button>
+        <button className="next-btn" onClick={btnpressnxt}><p>&gt;</p></button>
+       
+       <div className="product-container">
+        <Mycard  cardno ="1" />
+         <Mycard  cardno="2"/>
+          <Mycard cardno="3" />
+           <Mycard  cardno ="4"/>
+            <Mycard cardno="6" />
+            <Mycard cardno="7" />
+            <Mycard cardno="8" />
+            <Mycard cardno="9" />
+            <Mycard cardno="10" />
+            <Mycard cardno="11" />
+            <Mycard cardno="12" />
+            <Mycard cardno="13" />
+            <Mycard cardno="14" />
+            <Mycard cardno="15" />
+            <Mycard cardno="16" />
+        </div> </div>
     )
 }
